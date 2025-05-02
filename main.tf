64ab7cfa8fac8d99bf7c1205cf9963f72070b4e7
gituser4620 <shaikayesh7@gmail.com>
2025-05-02 15:39:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "ayesha-demo-bucket-jenkins"
   acl    = "private"
 }
+provider "aws" {
+  region  = "us-west-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
