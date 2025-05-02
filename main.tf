c788db399eab64507011a1e4e5f1719f97e62fca
gituser4620 <shaikayesh7@gmail.com>
2025-05-02 15:40:26 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,18 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "ayesha-demo-bucket-jenkins"
   acl    = "private"
 }
+
+# Use alias for the second provider
 provider "aws" {
-  region  = "us-west-2"
+  region     = "us-west-2"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
+  alias      = "west"
 }
 
+# You can now use the aliased provider in other resources
+resource "aws_s3_bucket" "my_bucket_west" {
+  provider = aws.west  # Use the alias for the second provider
+  bucket   = "ayesha-demo-bucket-jenkins-west"
+  acl      = "private"
+}
