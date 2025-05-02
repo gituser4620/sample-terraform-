6c27983ad6206f2b59df77f081d16ed9c9ea7c88
gituser4620 <shaikayesh7@gmail.com>
2025-05-02 15:44:01 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,29 @@
+# Declare input variables for AWS credentials
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+  description = "AWS Access Key ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+  description = "AWS Secret Access Key"
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "ayesha-demo-bucket-jenkins"
+  # Remove deprecated acl argument here
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.bucket
   acl    = "private"
 }
 
-# Use alias for the second provider
+# Provider for a different region with credentials
 provider "aws" {
   region     = "us-west-2"
   access_key = var.AWS_ACCESS_KEY_ID
@@ -17,7 +33,12 @@ provider "aws" {
 
 # You can now use the aliased provider in other resources
 resource "aws_s3_bucket" "my_bucket_west" {
-  provider = aws.west  # Use the alias for the second provider
+  provider = aws.west
   bucket   = "ayesha-demo-bucket-jenkins-west"
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl_west" {
+  provider = aws.west
+  bucket   = aws_s3_bucket.my_bucket_west.bucket
   acl      = "private"
 }
